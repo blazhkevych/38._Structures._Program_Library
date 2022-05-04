02d5e2b3196f81e47c55e2480f0963ea68e6c454
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-05-04 15:54:04 +0300

    правки

--- task/lib.h
+++ task/lib.h
@@ -1,1 +1,8 @@
-﻿
+﻿struct Book
+{
+	char title[50]; // название книги
+	char author[50]; // автор книги
+	char publishingHouse[50]; // издательство книги
+	int yearOfPublishing; // год издания книги
+	char genre; // жанр книги
+};
